55933f36078b27836a4bfebc62c7a1f83bbfdccc
Jeanine Bonot <jeaninebonot@gmail.com>
2024-03-11 20:25:59 +0100

    feat: fetch gravatar data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,25 @@
-import Image from "next/image";
+import { NextPage } from "next";
+import { IGravatarProfile } from "./types";
 
-export default function Home() {
-  return <div>Hello world</div>;
-}
+interface IPageProps {}
+
+const getData = async () => {
+  const res = await fetch(
+    `https://www.gravatar.com/${process.env.EMAIL_HASH}.json`
+  );
+
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error("Failed to fetch data");
+  }
+
+  return await res.json();
+};
+
+const Home: NextPage<IPageProps> = async () => {
+  const data: IGravatarProfile = await getData();
+
+  return <div>{data.entry[0].full_name}</div>;
+};
+
+export default Home;
---
+++ src/app/types/index.ts
@@ -0,0 +1,25 @@
+export interface IGravatarProfile {
+  entry: {
+    hash: string;
+    requestHash: string;
+    profileUrl: string;
+    preferredUsername: string;
+    thumbnailUrl: string;
+    photos: {
+      value: string;
+      type: string;
+    }[];
+    last_profile_edit: string;
+    displayName: string;
+    name: {
+      givenName: string;
+      familyName: string;
+      formatted: string;
+    };
+    full_name: string;
+    urls: {
+      title: string;
+      value: string;
+    }[];
+  }[];
+}
